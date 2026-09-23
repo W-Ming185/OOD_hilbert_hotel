f91f357c4917eed8f7d7faac1c56994610f6db8d
Ppi2613 <Peerawich2613@gmail.com>
2026-09-24 00:12:09 +0700

    fix class

--- class_Building.py
+++ class_Building.py
@@ -1,12 +1,17 @@
 class Building:
     def __int__(self,node_id):
         self.__node_id = node_id
-        self.__RoomAdrr = []
+        self.__RoomAddr = []
         self.__Vnode = []
     
     @property
     def get_node_id(self):
         return self.__node_id
-
+    @property
+    def get_RoomAddr(self):
+        return self.__RoomAddr
+    @property
+    def get_Vnode(self):
+        return self.__Vnode
     def add_room(self):
         pass
