57bc79c30405330fe7bf62bec0105f2e56ba0d87
Popo-Jia <5412popo@gmail.com>
2026-09-24 00:36:56 +0700

    Remove next and prev because we change from linked list to normal list

--- vnode.py
+++ vnode.py
@@ -1,19 +1,8 @@
 class VNode:
-    def __init__(self,hash_value):
+    def __init__(self,hash_value,building):
         self.__hash_value = hash_value
+        self.__building = building
         self.__node_seq = None
-        self.__building = None
-        self.__next = None
-        self.__prev = None
-
-    def assign_next(self,node):
-        pass
-
-    def assign_prev(self,node):
-        pass
-
-    def assign_building(self,building):
-        pass
 
     def assign_node_seq(self,rank):
         pass
