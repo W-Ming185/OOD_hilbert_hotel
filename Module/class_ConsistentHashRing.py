025924cea1f02824b92918c6f86c567bf4caa92b
Ppi2613 <Peerawich2613@gmail.com>
2026-09-24 22:56:54 +0700

    class_hasmod

--- Module/class_ConsistentHashRing.py
+++ Module/class_ConsistentHashRing.py
@@ -1,11 +1,15 @@
+import bisect
+
 class ConsistentHashRing:
-    def __int__(self,Vnode):
-        self.__Vnode = Vnode
+    def __init__(self):
+        self.__Vnode = []
     @property
     def get_Vnode(self):
         return self.__Vnode
     
-    def add_node(self):
-        pass
+    def add_node(self,building,):
+        if self.__Vnode is None:
+            pass
+        bisect.insort(self.__Vnode,data)
     def remove_node(self):
         pass
---
+++ Module/class_hasmod.py
@@ -0,0 +1,8 @@
+import bisect
+class Hashmod:
+    def __init__(self):
+        self.__building = []
+    def add_building(self,data):
+        pass
+    def remove_building(self,data):
+        pass
