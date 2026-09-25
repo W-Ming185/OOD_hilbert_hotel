57dcacd7cea79718bbe347f19f8484a4962c6cdf
Ppi2613 <Peerawich2613@gmail.com>
2026-09-25 22:02:16 +0700

    class-hasmodn

--- Module/class_hasmod.py
+++ Module/class_hasmod.py
@@ -1,8 +1,10 @@
-import bisect
 class Hashmod:
     def __init__(self):
         self.__building = []
-    def add_building(self,data):
-        pass
-    def remove_building(self,data):
-        pass
+    def add_building(self, data):
+        index = data % len(self.__building)
+        self.__building[index].append(data)
+    def remove_building(self, data):
+        index = data % len(self.__building)
+        if data in self.__building[index]:
+            self.__building[index].remove(data)
